9a61589e535f87937281117562eafffdcdb9d714
Suprianto Djamalu <erthru@Supriantos-MacBook-Pro.local>
2021-02-25 23:55:06 +0800

    new example route added

--- src/routes.ts
+++ src/routes.ts
@@ -6,5 +6,6 @@ const router = Router();
 
 router.post("/auth/login", authRepository.login);
 router.get("/auth", checkAuth([AuthLevel.ADMIN, AuthLevel.USER]).verify, authRepository.get);
+router.get("/auth/admin-only", checkAuth([AuthLevel.ADMIN]).verify, authRepository.get);
 
 export default router;
